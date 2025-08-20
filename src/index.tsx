115a9e7d4a2028ab3d71d9980f0704adb0356883
BlocksFunction <105109978+BlocksFunction@users.noreply.github.com>
2025-08-20 12:40:06 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,4 @@
 import { render } from "preact";
 import AppPage from "./app";
 
-export function App() {
-    return <AppPage />;
-}
-
-render(<App />, document.getElementById("root"));
+render(<AppPage />, document.getElementById("root"));
