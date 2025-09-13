4f31853a0e74e5ec393b80416d1336c3d85152f1
BlocksFunction <105109978+BlocksFunction@users.noreply.github.com>
2025-09-13 18:19:04 +0800

    Update ThemeMode.tsx

--- src/components/ThemeMode.tsx
+++ src/components/ThemeMode.tsx
@@ -36,6 +36,7 @@ export default function ThemeMode() {
             setIsDark(!isDark);
             return;
         }
+        return;
 
         const endRadius = Math.hypot(
             Math.max(x, window.innerWidth - x),
